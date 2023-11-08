0e2753531d04791cea7e71591c7acc0f8abafc8e
tantaihaha4487 <tantaiiottwo@gmail.com>
2023-11-08 23:20:41 +0700

    first

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask, redirect, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index(): 
+    return redirect("/login")
+
+@app.route("/login")
+def login(): 
+    return render_template("login.html")
+
+@app.route("/signup")
+def signup(): 
+    return render_template("signup.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
